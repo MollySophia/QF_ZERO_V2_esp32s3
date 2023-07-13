44748d991017a4de740b2314694b941cc4962852
Molly Sophia <mollysophia379@gmail.com>
2023-07-13 10:37:36 +0800

    UI: Use custom launcher
    Signed-off-by: Molly Sophia <mollysophia379@gmail.com>

--- main/ui/main_ui.cpp
+++ main/ui/main_ui.cpp
@@ -7,13 +7,19 @@
 #endif
 
 #include "mooncake.h"
+#include "apps/launcher/launcher.h"
 #include "main_ui.h"
 
 MOONCAKE::Mooncake mooncake;
+MOONCAKE::LAUNCHER::Launcher* launcher = new MOONCAKE::LAUNCHER::Launcher;
 
 void main_ui_create() {
     // lv_demo_music();
+    MOONCAKE::FrameworkConfig_t config = mooncake.config();
+    config.playBootAnim = false;
+    mooncake.config(config);
     mooncake.setDisplay(240, 240);
+    mooncake.setLauncher(launcher);
     mooncake.init();
     mooncake.installBuiltinApps();
     return;
